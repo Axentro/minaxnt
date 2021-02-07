17427f6110646ac4c41e77a679d92614cdc9bae4
Christian Kakesa <christian.kakesa@gmail.com>
2021-02-07 01:06:47 +0100

    Add client core version

--- util/welcome.go
+++ util/welcome.go
@@ -3,6 +3,7 @@ package util
 import (
 	"fmt"
 	"minaxnt/miner"
+	"minaxnt/types"
 )
 
 func Welcome(client *miner.Client) {
@@ -19,13 +20,14 @@ func Welcome(client *miner.Client) {
  `
 	fmt.Print(str)
 	fmt.Println()
-	fmt.Println("=>        miner name:", client.ClientName)
-	fmt.Println("=>          miner id:", client.MinerID)
-	fmt.Println("=> connected to node:", client.NodeURL)
-	fmt.Println("=>     miner address:", client.Address)
-	fmt.Println("=>     miner process:", client.Process)
-	fmt.Println("=>         cpu model:", client.CPUModel)
-	fmt.Println("=>         cpu cores:", client.CPUCores)
-	fmt.Println("=>        cpu caches:", client.CPUCaches)
-	fmt.Println("=>      cpu features:", client.CPUFeatures)
+	fmt.Println("=>          miner name:", client.ClientName)
+	fmt.Println("=>            miner id:", client.MinerID)
+	fmt.Println("=> client core version:", types.CoreVersion)
+	fmt.Println("=>   connected to node:", client.NodeURL)
+	fmt.Println("=>       miner address:", client.Address)
+	fmt.Println("=>       miner process:", client.Process)
+	fmt.Println("=>           cpu model:", client.CPUModel)
+	fmt.Println("=>           cpu cores:", client.CPUCores)
+	fmt.Println("=>          cpu caches:", client.CPUCaches)
+	fmt.Println("=>        cpu features:", client.CPUFeatures)
 }
